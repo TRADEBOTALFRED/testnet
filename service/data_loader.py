a22518374fafad1c912e9db2ce66f75eec524f72
Oleg <otrifonow@gmail.com>
2018-08-12 10:15:09 +0400

    retro downloader tuning

--- service/data_loader.py
+++ service/data_loader.py
@@ -7,7 +7,7 @@ from .markets import binance_api
 from datetime import datetime, timedelta
 from django.utils import timezone
 
-MAX_DOWNLOADS_PER_TIME = 100
+MAX_DOWNLOADS_PER_TIME = 10000
 available_to_download_per_time = MAX_DOWNLOADS_PER_TIME
 
 
